2423fa272406cdc5bbee2bc06eeef29f1f8000df
dev15 <dev15@7RASAM.tplduck.sewp.nasa.gov>
2015-05-11 13:36:26 -0400

    Add instruction comments on where to add code to UserDAO.java

--- src/main/java/course/UserDAO.java
+++ src/main/java/course/UserDAO.java
@@ -45,13 +45,18 @@ public class UserDAO {
         String passwordHash = makePasswordHash(password, Integer.toString(random.nextInt()));
 
         // XXX WORK HERE
+        // create an object suitable for insertion into the user collection
+        // be sure to add username and hashed password to the document. problem instructions
+        // will tell you the schema that the documents must follow.
 
         if (email != null && !email.equals("")) {
             // XXX WORK HERE
+          // if there is an email address specified, add it to the document too.
         }
 
         try {
             // XXX WORK HERE
+            // insert the document into the user collection here
             return true;
         } catch (MongoWriteException e) {
             if (e.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
@@ -65,7 +70,8 @@ public class UserDAO {
     public Document validateLogin(String username, String password) {
         Document user = null;
 
-        // XXX 
+        // XXX  look in the user collection for a user that has this username
+        // assign the result to the user variable.
 
         if (user == null) {
             System.out.println("User not in database");
